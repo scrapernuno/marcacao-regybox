fb00703d17e0a2320b6059a60c5990613851bdb1
scrapernuno <frenuno@gmail.com>
2026-07-21 23:11:07 +0100

    Change DIAS_ANTECEDENCIA from 3 to 4

--- marcar_aula.py
+++ marcar_aula.py
@@ -37,7 +37,7 @@ PASSWORD = os.environ.get("REGYBOX_PASS", "").strip()
 TIMEZONE = ZoneInfo("Atlantic/Madeira")
 
 # A aula é procurada quatro dias depois da execução.
-DIAS_ANTECEDENCIA = 3
+DIAS_ANTECEDENCIA = 4
 
 HORA_ALVO = "18:25"
 
